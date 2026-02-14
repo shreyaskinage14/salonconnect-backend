e6943632762e6d4f143556f53af5a1c6ccbb923c
shreyas kinage <shreyaskinagee@gmail.com>
2026-02-15 01:16:11 +0530

    db url changes

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://salon:lTss13uzfPYOgo0nqEceBkXr60YOWHfR@dpg-d58njs75r7bs738qtp90-a.virginia-postgres.render.com/salon_db_0czk")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://salon:u7LK1FC4yLg2Joc8qNwnch3UPNjTMfuq@dpg-d68cvn3h46gs73fd84qg-a.virginia-postgres.render.com/salon_db_cz7z")
 
 # echo=True for SQL logging (handy while learning)
 engine = create_engine(DATABASE_URL, echo=False, future=True)
